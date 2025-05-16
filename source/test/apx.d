cac05c737777ae4f3d3246e4537b0d7c32cd34ed
deepprog <deepprog@gmail.com>
2025-05-16 09:48:12 +0900

    Fix

--- source/test/apx.d
+++ source/test/apx.d
@@ -8,10 +8,8 @@ import std.exception;
 
 import xbyak;
 
-version(X86_64)
-{
-	version = XBYAK64;
-}
+version(X86)    version = XBYAK32;
+version(X86_64) version = XBYAK64;
 
 version(XBYAK64)
 {
@@ -90,14 +88,22 @@ void reg64()
 	{
 		this()
 		{
-			adc(r30, rax); adc(r30, rcx); adc(r30, rdx); adc(r30, rbx); adc(r30, rsp); adc(r30, rbp); adc(r30, rsi); adc(r30, rdi);
-			adc(r30, r8); adc(r30, r9); adc(r30, r10); adc(r30, r11); adc(r30, r12); adc(r30, r13); adc(r30, r14); adc(r30, r15);
-			adc(r30, r16); adc(r30, r17); adc(r30, r18); adc(r30, r19); adc(r30, r20); adc(r30, r21); adc(r30, r22); adc(r30, r23);
-			adc(r30, r24); adc(r30, r25); adc(r30, r26); adc(r30, r27); adc(r30, r28); adc(r30, r29); adc(r30, r30); adc(r30, r31);
-			adc(rax, r30); adc(rcx, r30); adc(rdx, r30); adc(rbx, r30); adc(rsp, r30); adc(rbp, r30); adc(rsi, r30); adc(rdi, r30);
-			adc(r8, r30); adc(r9, r30); adc(r10, r30); adc(r11, r30); adc(r12, r30); adc(r13, r30); adc(r14, r30); adc(r15, r30);
-			adc(r16, r30); adc(r17, r30); adc(r18, r30); adc(r19, r30); adc(r20, r30); adc(r21, r30); adc(r22, r30); adc(r23, r30);
-			adc(r24, r30); adc(r25, r30); adc(r26, r30); adc(r27, r30); adc(r28, r30); adc(r29, r30); adc(r30, r30); adc(r31, r30);
+			adc(r30, rax); adc(r30, rcx); adc(r30, rdx); adc(r30, rbx);
+			adc(r30, rsp); adc(r30, rbp); adc(r30, rsi); adc(r30, rdi);
+			adc(r30, r8); adc(r30, r9); adc(r30, r10); adc(r30, r11);
+			adc(r30, r12); adc(r30, r13); adc(r30, r14); adc(r30, r15);
+			adc(r30, r16); adc(r30, r17); adc(r30, r18); adc(r30, r19);
+			adc(r30, r20); adc(r30, r21); adc(r30, r22); adc(r30, r23);
+			adc(r30, r24); adc(r30, r25); adc(r30, r26); adc(r30, r27);
+			adc(r30, r28); adc(r30, r29); adc(r30, r30); adc(r30, r31);
+			adc(rax, r30); adc(rcx, r30); adc(rdx, r30); adc(rbx, r30);
+			adc(rsp, r30); adc(rbp, r30); adc(rsi, r30); adc(rdi, r30);
+			adc(r8, r30); adc(r9, r30); adc(r10, r30); adc(r11, r30);
+			adc(r12, r30); adc(r13, r30); adc(r14, r30); adc(r15, r30);
+			adc(r16, r30); adc(r17, r30); adc(r18, r30); adc(r19, r30);
+			adc(r20, r30); adc(r21, r30); adc(r22, r30); adc(r23, r30);
+			adc(r24, r30); adc(r25, r30); adc(r26, r30); adc(r27, r30);
+			adc(r28, r30); adc(r29, r30); adc(r30, r30); adc(r31, r30);
 		}
 	}
 	const uint8_t[] tbl = [
@@ -141,14 +147,22 @@ void reg32()
 	{
 		this()
 		{
-			adc(r30d, eax); adc(r30d, ecx); adc(r30d, edx); adc(r30d, ebx); adc(r30d, esp); adc(r30d, ebp); adc(r30d, esi); adc(r30d, edi);
-			adc(r30d, r8d); adc(r30d, r9d); adc(r30d, r10d); adc(r30d, r11d); adc(r30d, r12d); adc(r30d, r13d); adc(r30d, r14d); adc(r30d, r15d);
-			adc(r30d, r16d); adc(r30d, r17d); adc(r30d, r18d); adc(r30d, r19d); adc(r30d, r20d); adc(r30d, r21d); adc(r30d, r22d); adc(r30d, r23d);
-			adc(r30d, r24d); adc(r30d, r25d); adc(r30d, r26d); adc(r30d, r27d); adc(r30d, r28d); adc(r30d, r29d); adc(r30d, r30d); adc(r30d, r31d);
-			adc(eax, r29d); adc(ecx, r29d); adc(edx, r29d); adc(ebx, r29d); adc(esp, r29d); adc(ebp, r29d); adc(esi, r29d); adc(edi, r29d);
-			adc(r8d, r29d); adc(r9d, r29d); adc(r10d, r29d); adc(r11d, r29d); adc(r12d, r29d); adc(r13d, r29d); adc(r14d, r29d); adc(r15d, r29d);
-			adc(r16d, r29d); adc(r17d, r29d); adc(r18d, r29d); adc(r19d, r29d); adc(r20d, r29d); adc(r21d, r29d); adc(r22d, r29d); adc(r23d, r29d);
-			adc(r24d, r29d); adc(r25d, r29d); adc(r26d, r29d); adc(r27d, r29d); adc(r28d, r29d); adc(r29d, r29d); adc(r30d, r29d); adc(r31d, r29d);
+			adc(r30d, eax); adc(r30d, ecx); adc(r30d, edx); adc(r30d, ebx);
+			adc(r30d, esp); adc(r30d, ebp); adc(r30d, esi); adc(r30d, edi);
+			adc(r30d, r8d); adc(r30d, r9d); adc(r30d, r10d); adc(r30d, r11d);
+			adc(r30d, r12d); adc(r30d, r13d); adc(r30d, r14d); adc(r30d, r15d);
+			adc(r30d, r16d); adc(r30d, r17d); adc(r30d, r18d); adc(r30d, r19d);
+			adc(r30d, r20d); adc(r30d, r21d); adc(r30d, r22d); adc(r30d, r23d);
+			adc(r30d, r24d); adc(r30d, r25d); adc(r30d, r26d); adc(r30d, r27d);
+			adc(r30d, r28d); adc(r30d, r29d); adc(r30d, r30d); adc(r30d, r31d);
+			adc(eax, r29d); adc(ecx, r29d); adc(edx, r29d); adc(ebx, r29d);
+			adc(esp, r29d); adc(ebp, r29d); adc(esi, r29d); adc(edi, r29d);
+			adc(r8d, r29d); adc(r9d, r29d); adc(r10d, r29d); adc(r11d, r29d);
+			adc(r12d, r29d); adc(r13d, r29d); adc(r14d, r29d); adc(r15d, r29d);
+			adc(r16d, r29d); adc(r17d, r29d); adc(r18d, r29d); adc(r19d, r29d);
+			adc(r20d, r29d); adc(r21d, r29d); adc(r22d, r29d); adc(r23d, r29d);
+			adc(r24d, r29d); adc(r25d, r29d); adc(r26d, r29d); adc(r27d, r29d);
+			adc(r28d, r29d); adc(r29d, r29d); adc(r30d, r29d); adc(r31d, r29d);
 		}
 	}
 	const uint8_t[] tbl = [
@@ -192,14 +206,22 @@ void reg16()
 	{
 		this()
 		{
-			adc(r30w, ax); adc(r30w, cx); adc(r30w, dx); adc(r30w, bx); adc(r30w, sp); adc(r30w, bp); adc(r30w, si); adc(r30w, di);
-			adc(r30w, r8w); adc(r30w, r9w); adc(r30w, r10w); adc(r30w, r11w); adc(r30w, r12w); adc(r30w, r13w); adc(r30w, r14w); adc(r30w, r15w);
-			adc(r30w, r16w); adc(r30w, r17w); adc(r30w, r18w); adc(r30w, r19w); adc(r30w, r20w); adc(r30w, r21w); adc(r30w, r22w); adc(r30w, r23w);
-			adc(r30w, r24w); adc(r30w, r25w); adc(r30w, r26w); adc(r30w, r27w); adc(r30w, r28w); adc(r30w, r29w); adc(r30w, r30w); adc(r30w, r31w);
-			adc(ax, r29w); adc(cx, r29w); adc(dx, r29w); adc(bx, r29w); adc(sp, r29w); adc(bp, r29w); adc(si, r29w); adc(di, r29w);
-			adc(r8w, r29w); adc(r9w, r29w); adc(r10w, r29w); adc(r11w, r29w); adc(r12w, r29w); adc(r13w, r29w); adc(r14w, r29w); adc(r15w, r29w);
-			adc(r16w, r29w); adc(r17w, r29w); adc(r18w, r29w); adc(r19w, r29w); adc(r20w, r29w); adc(r21w, r29w); adc(r22w, r29w); adc(r23w, r29w);
-			adc(r24w, r29w); adc(r25w, r29w); adc(r26w, r29w); adc(r27w, r29w); adc(r28w, r29w); adc(r29w, r29w); adc(r30w, r29w); adc(r31w, r29w);
+			adc(r30w, ax); adc(r30w, cx); adc(r30w, dx); adc(r30w, bx);
+			adc(r30w, sp); adc(r30w, bp); adc(r30w, si); adc(r30w, di);
+			adc(r30w, r8w); adc(r30w, r9w); adc(r30w, r10w); adc(r30w, r11w);
+			adc(r30w, r12w); adc(r30w, r13w); adc(r30w, r14w); adc(r30w, r15w);
+			adc(r30w, r16w); adc(r30w, r17w); adc(r30w, r18w); adc(r30w, r19w);
+			adc(r30w, r20w); adc(r30w, r21w); adc(r30w, r22w); adc(r30w, r23w);
+			adc(r30w, r24w); adc(r30w, r25w); adc(r30w, r26w); adc(r30w, r27w);
+			adc(r30w, r28w); adc(r30w, r29w); adc(r30w, r30w); adc(r30w, r31w);
+			adc(ax, r29w); adc(cx, r29w); adc(dx, r29w); adc(bx, r29w);
+			adc(sp, r29w); adc(bp, r29w); adc(si, r29w); adc(di, r29w);
+			adc(r8w, r29w); adc(r9w, r29w); adc(r10w, r29w); adc(r11w, r29w);
+			adc(r12w, r29w); adc(r13w, r29w); adc(r14w, r29w); adc(r15w, r29w);
+			adc(r16w, r29w); adc(r17w, r29w); adc(r18w, r29w); adc(r19w, r29w);
+			adc(r20w, r29w); adc(r21w, r29w); adc(r22w, r29w); adc(r23w, r29w);
+			adc(r24w, r29w); adc(r25w, r29w); adc(r26w, r29w); adc(r27w, r29w);
+			adc(r28w, r29w); adc(r29w, r29w); adc(r30w, r29w); adc(r31w, r29w);
 		}
 	}
 	const uint8_t[] tbl = [
@@ -247,14 +269,22 @@ void reg8()
 	{
 		this()
 		{
-			adc(r17b, al); adc(r17b, cl); adc(r17b, dl); adc(r17b, bl); adc(r17b, spl); adc(r17b, bpl); adc(r17b, sil); adc(r17b, dil);
-			adc(r17b, r8b); adc(r17b, r9b); adc(r17b, r10b); adc(r17b, r11b); adc(r17b, r12b); adc(r17b, r13b); adc(r17b, r14b); adc(r17b, r15b);
-			adc(r17b, r16b); adc(r17b, r17b); adc(r17b, r18b); adc(r17b, r19b); adc(r17b, r20b); adc(r17b, r21b); adc(r17b, r22b); adc(r17b, r23b);
-			adc(r17b, r24b); adc(r17b, r25b); adc(r17b, r26b); adc(r17b, r27b); adc(r17b, r28b); adc(r17b, r29b); adc(r17b, r30b); adc(r17b, r31b);
-			adc(al, r20b); adc(cl, r20b); adc(dl, r20b); adc(bl, r20b); adc(spl, r20b); adc(bpl, r20b); adc(sil, r20b); adc(dil, r20b);
-			adc(r8b, r20b); adc(r9b, r20b); adc(r10b, r20b); adc(r11b, r20b); adc(r12b, r20b); adc(r13b, r20b); adc(r14b, r20b); adc(r15b, r20b);
-			adc(r16b, r20b); adc(r17b, r20b); adc(r18b, r20b); adc(r19b, r20b); adc(r20b, r20b); adc(r21b, r20b); adc(r22b, r20b); adc(r23b, r20b);
-			adc(r24b, r20b); adc(r25b, r20b); adc(r26b, r20b); adc(r27b, r20b); adc(r28b, r20b); adc(r29b, r20b); adc(r30b, r20b); adc(r31b, r20b);
+			adc(r17b, al); adc(r17b, cl); adc(r17b, dl); adc(r17b, bl);
+			adc(r17b, spl); adc(r17b, bpl); adc(r17b, sil); adc(r17b, dil);
+			adc(r17b, r8b); adc(r17b, r9b); adc(r17b, r10b); adc(r17b, r11b);
+			adc(r17b, r12b); adc(r17b, r13b); adc(r17b, r14b); adc(r17b, r15b);
+			adc(r17b, r16b); adc(r17b, r17b); adc(r17b, r18b); adc(r17b, r19b);
+			adc(r17b, r20b); adc(r17b, r21b); adc(r17b, r22b); adc(r17b, r23b);
+			adc(r17b, r24b); adc(r17b, r25b); adc(r17b, r26b); adc(r17b, r27b);
+			adc(r17b, r28b); adc(r17b, r29b); adc(r17b, r30b); adc(r17b, r31b);
+			adc(al, r20b); adc(cl, r20b); adc(dl, r20b); adc(bl, r20b);
+			adc(spl, r20b); adc(bpl, r20b); adc(sil, r20b); adc(dil, r20b);
+			adc(r8b, r20b); adc(r9b, r20b); adc(r10b, r20b); adc(r11b, r20b);
+			adc(r12b, r20b); adc(r13b, r20b); adc(r14b, r20b); adc(r15b, r20b);
+			adc(r16b, r20b); adc(r17b, r20b); adc(r18b, r20b); adc(r19b, r20b);
+			adc(r20b, r20b); adc(r21b, r20b); adc(r22b, r20b); adc(r23b, r20b);
+			adc(r24b, r20b); adc(r25b, r20b); adc(r26b, r20b); adc(r27b, r20b);
+			adc(r28b, r20b); adc(r29b, r20b); adc(r30b, r20b); adc(r31b, r20b);
 		}
 	}
 	const uint8_t[] tbl = [
