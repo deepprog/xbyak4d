77bcd6280bb7a6d59134f948293608eed53aace2
deepprog <deepprog@gmail.com>
2025-06-12 12:21:09 +0900

    Update comp.d

--- source/test/avx10/comp.d
+++ source/test/avx10/comp.d
@@ -7,80 +7,47 @@ import std.stdint;
 import std.exception;
 import xbyak;
 
+version (X86) version = XBYAK32;
+version (X86_64) version = XBYAK64;
 
-class Code : CodeGenerator
+version (XBYAK64)
 {
-    this()
+
+    @("xed_comp")
+    unittest
     {
-        super(4096*8);
-        setDefaultEncodingAVX10(AVX10v2Encoding);
-        vcomxsd(xm1, xm2|T_sae);  dump(); size_ = 0;
-        vcomxsd(xm1, ptr[rax+128]);  dump(); size_ = 0;
-
-        vcomxsh(xm1, xm2|T_sae);  dump(); size_ = 0;
-        vcomxsh(xm1, ptr[rax+128]);  dump(); size_ = 0;
-
-        vcomxss(xm1, xm2|T_sae);  dump(); size_ = 0;
-        vcomxss(xm1, ptr[rax+128]);  dump(); size_ = 0;
-
-        vucomxsd(xm1, xm2|T_sae);  dump(); size_ = 0;
-        vucomxsd(xm1, ptr[rax+128]);  dump(); size_ = 0;
-
-        vucomxsh(xm1, xm2|T_sae);  dump(); size_ = 0;
-        vucomxsh(xm1, ptr[rax+128]);  dump(); size_ = 0;
-
-        vucomxss(xm1, xm2|T_sae);  dump(); size_ = 0;
-        vucomxss(xm1, ptr[rax+128]);  dump(); size_ = 0;
+        xed_comp();
     }
-}
 
-void Xdump(uint8_t* p, size_t bufSize) 
-{
-    size_t remain  = bufSize;
-    for (int i = 0; i < 4; i++) {
-        size_t disp = 16;
-        if (remain < 16) {
-            disp = remain;
-        }
-        for (size_t j = 0; j < 16; j++) {
-            if (j < disp) {
-                write(format("%02X", p[i * 16 + j]));
-            }
-        }
-        writeln();
-        remain -= disp;
-        if (remain <= 0) {
-            break;
-        }
-    }
-}
-
-@("xed_comp")
-unittest
-{
-    xed_comp();
-}
-
-void xed_comp()
-{
-    //try
-    //{
-    //    writeln("comp");
+    void xed_comp()
+    {
+        writeln("xed_comp");
         scope Code c = new Code();
-        auto tbl = c.getCode();
-    
-        //writeln(c.getSize());    
-        //Xdump(tbl, c.getSize());
+    }
 
-    //    writeln("end comp");
-    //    FILE *fp = fopen("bin", "wb");
-    //    if (fp) {
-    //        fwrite(c.getCode(), 1, c.getSize(), fp);
-    //        fclose(fp);
-    //} 
-    //catch (Exception e)
-    //{
-    //    printf("ERR %s\n", e.what());
-    //}
+    class Code : CodeGenerator
+    {
+        this()
+        {
+            super(4096 * 8);
+            setDefaultEncodingAVX10(AVX10v2Encoding);
+            vcomxsd(xm1, xm2 | T_sae);
+            vcomxsd(xm1, ptr[rax + 128]);
+
+            vcomxsh(xm1, xm2 | T_sae);
+            vcomxsh(xm1, ptr[rax + 128]);
+
+            vcomxss(xm1, xm2 | T_sae);
+            vcomxss(xm1, ptr[rax + 128]);
+
+            vucomxsd(xm1, xm2 | T_sae);
+            vucomxsd(xm1, ptr[rax + 128]);
+
+            vucomxsh(xm1, xm2 | T_sae);
+            vucomxsh(xm1, ptr[rax + 128]);
+
+            vucomxss(xm1, xm2 | T_sae);
+            vucomxss(xm1, ptr[rax + 128]);
+        }
+    }
 }
-
